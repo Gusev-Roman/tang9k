5413720a9a2af1b3ae17e56ef0cbd304fe01aad7
RGusev <roman.gusev@brsmed.com>
2025-03-13 19:03:02 +0300

    tristate buffer implemented

--- lesson2/psram_qspi.v
+++ lesson2/psram_qspi.v
@@ -4,39 +4,59 @@
 module psram999
 (
     input clk,
-    output led1,    // ready to work
+    output reg led1,    // ready to work
     inout [3:0] data,
     input [22:0] addr, // top input cannot be a register
-    input cs_n,     // pull-up
-    output ready    // data moved to internal registry
+    output reg rcs,     // pull-up
+    output reg ready    // data moved to internal registry
 );
 
-localparam STARTUP_TIME = 4055; // ticks to init, 150us
+localparam STARTUP_TIME = 4; // 4060 ticks to init, 150us + 50ns
 localparam RSTEN = 8'h66;
 localparam RST = 8'h99;
 
+wire [3:0] data_in;
+wire [3:0] data_out;
 reg [11:0] tick_cnt = 12'b0;
 reg [7:0] cmd;
 reg [5:0] state;
+(* lc_debug *)
+reg mosi;
+// сигнал enable должен формироваться внутри модуля, в зависимости от времени и количества отправленных бит
+(* lc_debug *)
+reg enable;
 
-assign data = 4'bz;   // not driven
-//assign cs_n = 1;   // not active
-assign state = 0;  // wait_for_ready
+// если enable=0, data_in===data, иначе data_in===Z
+assign data_in = ~enable ? data : 4'bzzzz;
+// если enable=0, data_out===Z, иначе data_out===data
+assign data_out = enable ? data : 4'bzzzz;
+// если enable=0, data===data_in, иначе data===data_out 
+assign data = enable ? data_out : data_in;
+// меняя значение mosi и enable можно менять линию data
+assign data_out[0] = mosi;
 
 
 always @(posedge clk) begin
+    if(tick_cnt == 0) begin
+        state <= 0;
+        mosi <= 0;
+        enable <= 1;
+        ready <=0;
+        rcs <= 1;
+    end
     if(tick_cnt < STARTUP_TIME) begin
         tick_cnt = tick_cnt + 12'b1;
         led1 <= 1;
     end else begin
         led1 <= 0; // ready to work, 
+
         if(state == 0) begin
             state <= 1;
+            mosi <= 1;
         end else if(state == 1) begin
         // start to send "SELECT_QSPI" cmd
         end
     end
-
 end
 
 //assign led1 = 0;   // initial off
--- lesson2/top.v
+++ lesson2/top.v
@@ -8,9 +8,10 @@ module top
     input clk,          // HSE clock, 27 MHz
     output [5:0] led,    // LEDs to indicate state
     output rclk,         // clock to PSRAM
-    inout [3:0] data,    // data lines to PSRAM. Started as 1-wire
+    inout wire [3:0] data,    // data lines to PSRAM. Started as 1-wire
     output rcs
 );
+
 
 reg [1:0] key_s_rise; // ?
 reg [22:0] haddr = 23'b0;   // address init as 0
@@ -23,17 +24,18 @@ psram999 mypsram(
     .led1(led[0]),
     .addr(haddr),
     .data(data),
-    .cs_n(rcs)
+    .rcs(rcs)
 );
 
 //BUFG one_bufg(
 //    .I(clk),
 //    .O(rclk)
 //);
-
+/*
 DQCE dqce_one(
     .CLKIN(clk),
     .CLKOUT(rclk),
     .CE(rce)
 );
+*/
 endmodule
